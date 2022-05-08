8b753e5599eabaf7c0ae80c5c25d14d8b0d2f638
Mardoqueu <mdq767@gmail.com>
2022-05-08 12:32:39 -0300

    Componente NAV criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,79 +27,29 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 
-// Componentes do projeto
+//Componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
-import { LoginComponent } from './components/login/login.component';
-import { ToastrModule } from 'ngx-toastr';
-import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
-import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
-import { NgxMaskModule } from 'ngx-mask';
-import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
-import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecnico-delete.component';
-import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
-import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
-import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
-import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
-import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
-import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
-import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
-import { ChamadoReadComponent } from './components/chamado/chamado-read/chamado-read.component';
+
+// Componentes do projeto
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    HomeComponent,
-    HeaderComponent,
-    TecnicoListComponent,
-    LoginComponent,
-    TecnicoCreateComponent,
-    TecnicoUpdateComponent,
-    TecnicoDeleteComponent,
-    ClienteCreateComponent,
-    ClienteDeleteComponent,
-    ClienteListComponent,
-    ClienteUpdateComponent,
-    ChamadoListComponent,
-    ChamadoCreateComponent,
-    ChamadoUpdateComponent,
-    ChamadoReadComponent
+    NavComponent
+   
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    // Forms
-    FormsModule,
-    ReactiveFormsModule,
-    // Requisições http
-    HttpClientModule,
-    // Angular Material
-    MatFormFieldModule,
-    MatPaginatorModule,
-    MatCheckboxModule,
-    MatSnackBarModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatInputModule,
-    MatRadioModule,
-    MatTableModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    ToastrModule.forRoot({
-      timeOut: 4000,
-      closeButton: true,
-      progressBar: true
-    }),
-    NgxMaskModule.forRoot()
+  imports: [   
+   BrowserModule,
+   AppRoutingModule,
+   BrowserAnimationsModule,
+   //Forms
+   FormsModule,
+   ReactiveFormsModule,
+   //Requisições http
+   HttpClientModule,
   ],
-  providers: [AuthInterceptorProvider],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
