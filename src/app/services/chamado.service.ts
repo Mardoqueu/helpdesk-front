ccbe54e4c854cb1a085702daa6f70e7065157c8c
Mardoqueu <mdq767@gmail.com>
2022-05-19 14:22:49 -0300

    Validação de formulários para CHAMADO-CREATE

--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -11,7 +11,15 @@ export class ChamadoService {
 
   constructor(private http: HttpClient) {}
     
-  findAll(): Observable<Chamado[]>{
+  findAll(): Observable<Chamado[]> {
     return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
-  }  
+  }
+
+  create(chamado: Chamado): Observable<Chamado> {
+    return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado);
+  }
+
+  update(chamado: Chamado): Observable<Chamado> {
+    return this.http.put<Chamado>(`${API_CONFIG.baseUrl}/chamados/${chamado.id}`, chamado);
+  }
 }
