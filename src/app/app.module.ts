6ddcb7998db83047aeae4fc2dac39385f60ea9e9
Mardoqueu <mdq767@gmail.com>
2022-05-19 13:11:34 -0300

    Componente CHAMADO-CREATE criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,8 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ChamadoListComponent } from './componets/chamado/chamado-list/chamado-list.component';
+import { ComponentComponent } from './component/component.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 
 
@@ -62,7 +64,9 @@ import { ChamadoListComponent } from './componets/chamado/chamado-list/chamado-l
     ClienteUpdateComponent,
     ClienteDeleteComponent,
     ClienteListComponent,
-    ChamadoListComponent
+    ChamadoListComponent,
+    ComponentComponent,
+    ChamadoCreateComponent
   ],
   imports: [   
     BrowserModule,
