e33c71b0692966ca8c725dad9e9723267e61e2ba
Mardoqueu <mdq767@gmail.com>
2022-05-20 13:00:54 -0300

    Rota para componente CHAMADO-UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecn
 import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 import { TecnicoListComponent } from './components/tecnicos/tecnico-list/tecnico-list.component';
 import { ChamadoListComponent } from './componets/chamado/chamado-list/chamado-list.component';
+import { ChamadoReadComponent } from './componets/chamado/chamado-read/chamado-read.component';
 import { ChamadoUpdateComponent } from './componets/chamado/chamado-update/chamado-update.component';
 import { TecnicoDeleteComponent } from './componets/tecnico/tecnico-delete/tecnico-delete.component';
 
@@ -39,6 +40,8 @@ const routes: Routes = [
       {path: 'chamados',            component: ChamadoListComponent},
       {path: 'chamados/create',     component: ChamadoCreateComponent},
       {path: 'chamados/update/:id',  component: ChamadoUpdateComponent},
+      {path: 'chamados/read/:id',   component: ChamadoReadComponent},
+
 
 
 
